0dfd9eec6a79a39d059e753523d3f8c63ed1055b
Randel Smith <randel@redaxle.com>
2019-04-16 12:22:50 -0500

    Add draggable

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Draggable from 'react-draggable';
 
-const title = 'My Minimal React Webpack Babel Setup';
+const title = 'My Minimal React Webpack Babel Setup d';
 
 console.log('ReactDOM');
 console.log(chrome.runtime.id);
@@ -9,7 +10,35 @@ console.log(chrome.runtime.id);
 const elem = document.createElement('div');
 document.body.appendChild(elem);
 
+const containerSyle = {
+  zIndex    : 1000000,
+  position  : 'fixed',
+  width     : 200,
+  height     : 300,
+  right     : 10,
+  top       : 10,
+  background: 'red'
+}
+
 ReactDOM.render(
-  <div>{title}</div>,
+  (
+  <div >
+   <Draggable handle="strong">
+     <div style={containerSyle} className="box no-cursor">
+       <strong className="cursor"><div>Drag here</div></strong>
+       <div>You must click my handle to drag me</div>
+     </div>
+     </Draggable>
+   <style dangerouslySetInnerHTML={{__html: `
+     .react-draggable, .cursor {
+       cursor: move;
+     }
+     .no-cursor {
+       cursor: auto;
+     }
+   `}} />
+   </div>
+   
+  ),
   elem
 );
